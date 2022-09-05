fc4f6e03d7c401784beecea81cad933351547aac
BARIS <BARIS@DESKTOP-6J5N8QV>
2022-09-05 14:51:36 +0300

    Add project files.

---
+++ src/demoProjects/Kodlama.io.Devs/WebAPI/WebAPI/Controllers/ProgrammingLanguageController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.programmingLanguage.Commands.createProgrammingLanguage;
+using Application.Features.programmingLanguage.Commands.DeleteProgrammingLanguage;
+using Application.Features.programmingLanguage.Commands.UpdateProgrammingLanguage;
+using Application.Features.programmingLanguage.Dtos;
+using Application.Features.programmingLanguage.Models;
+using Application.Features.programmingLanguage.Queries.GetListProgrammingLanguage;
+using Core.Application.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguageController : BaseController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+        [HttpDelete("Id")]
+        public async Task<IActionResult> Delete([FromBody] DeleteProgrammingLanguageCommand deleteProgrammingLanguageCommand)
+        {
+            DeletedProgrammingLanguageDto result = await Mediator.Send(deleteProgrammingLanguageCommand);
+            return Ok(result);
+        }
+        [HttpGet("getlist")]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingLanguageQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            ProgrammingLanguageListModel result = await Mediator.Send(getListBrandQuery);
+            return Ok(result);
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromQuery] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand )
+        {
+            UpdatedProgrammingLanguageDto result = await  Mediator.Send(updateProgrammingLanguageCommand);
+            return Ok(result);
+            
+        }
+    }
+}
+ 
